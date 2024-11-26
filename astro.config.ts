2be8ca2be625f4282dde13435fb3ce8f0540ae37
xilab3D <ngardan@xilab.tech>
2024-11-26 09:28:36 +0100

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-
+  site: 'https://xilabvercel.vercel.app/',
   integrations: [
     tailwind({
       applyBaseStyles: false,
